89e2773f67eacaeed9bb42f0f447a506dc33b578
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2025-04-29 17:51:11 +0530

    updated fulll

--- telugu_connects/js/init.js
+++ telugu_connects/js/init.js
@@ -1,21 +1,21 @@
 // Hamburger Animation
-jQuery(".hamburger").click(function() {
-    jQuery(".hamburger").toggleClass("is-active");
-    jQuery(".menu-wrap").toggleClass("is-open");
+jQuery(".hamburger").click(function () {
+  jQuery(".hamburger").toggleClass("is-active");
+  jQuery(".menu-wrap").toggleClass("is-open");
 });
 
 jQuery(document).ready(function($) {
-    var $name = $('.sub-menu')
-    let revealsubmenu = false
-    $('.subMenuTrig').click(function() {
-        if (revealsubmenu === false) {
-            $($name).slideUp(500);
-            $(this).find($name).slideToggle(500);
-            revealsubmenu = true
-        } else {
-            $(this).find($name).slideToggle(500);
-            $(this).siblings().find($name).slideUp(500);
-            revealsubmenu = false
-        }
-    });
-});
+  var $name = $('.sub-menu')
+  let revealsubmenu = false
+    $('.subMenuTrig').click(function(){
+    if (revealsubmenu === false) {
+      $($name).slideUp(500);
+      $(this).find($name).slideToggle(500);
+        revealsubmenu = true
+      } else {
+        $(this).find($name).slideToggle(500);
+        $(this).siblings().find($name).slideUp(500);
+        revealsubmenu = false
+    }
+  });
+  });
